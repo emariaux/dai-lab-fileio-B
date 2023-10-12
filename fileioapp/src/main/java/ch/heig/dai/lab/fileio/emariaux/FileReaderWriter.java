0d3f209b76aded6cfb4bdb36bf783f05592510d4
emariaux <114927416+emariaux@users.noreply.github.com>
2023-10-12 14:12:43 +0200

    Feature File Reader Writer

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/emariaux/FileReaderWriter.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/emariaux/FileReaderWriter.java
@@ -11,11 +11,23 @@ public class FileReaderWriter {
      * @param encoding
      * @return the content of the file as a String, or null if an error occurred.
      */
-    public String readFile(File file, Charset encoding) {
-        // TODO: Implement the method body here. 
+    public String readFile(File file, Charset encoding) throws IOException {
+        
         // Use the ...Stream and ...Reader classes from the java.io package.
         // Make sure to close the streams and readers at the end.
-        return null;
+
+        var reader = new BufferedReader(
+                new InputStreamReader(
+                        new FileInputStream(file),
+                        encoding));
+
+        String line;
+        String content ="";
+        while((line = reader.readLine()) != null) {
+            content += line;
+        }
+        reader.close();
+        return content;
     }
 
     /**
@@ -25,10 +37,21 @@ public class FileReaderWriter {
      * @param encoding the encoding to use
      * @return true if the file was written successfully, false otherwise
      */
-    public boolean writeFile(File file, String content, Charset encoding) {
-        // TODO: Implement the method body here. 
+    public boolean writeFile(File file, String content, Charset encoding) throws IOException {
+
         // Use the ...Stream and ...Reader classes from the java.io package.
         // Make sure to flush the data and close the streams and readers at the end.
+
+        var writer = new BufferedWriter(
+                new OutputStreamWriter(
+                        new FileOutputStream(file),
+                        encoding));
+
+        writer.write("\r\n");
+
+        writer.write(content);
+        writer.flush();
+        writer.close();
         return false;
     }
 }
